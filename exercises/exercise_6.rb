a6e49ddf55f9ed863271c7d550161327dca7f47b
Brian LaFond <brian.d.lafond@gmail.com>
2022-01-05 20:25:46 +0000

    exercise 6 complete

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,9 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store2.employees.create(first_name: "Brian", last_name: "LaFond", hourly_rate: 120)
+@store1.employees.create(first_name: "Kaitlin", last_name: "Churcher", hourly_rate: 200)
+@store1.employees.create(first_name: "Renee", last_name: "Mackey", hourly_rate: 110)
+@store1.employees.create(first_name: "Peter", last_name: "Ha", hourly_rate: 130)
+@store2.employees.create(first_name: "Jennie", last_name: "Huor", hourly_rate: 150)
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,13 @@
 class Store < ActiveRecord::Base
 
+  include ActiveModel::Validations
+  has_many :employees
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { greater_than_or_equal_to: 0 }
+  validate :mens_or_womens_apparel
+  def mens_or_womens_apparel
+    if mens_apparel.present? == false && womens_apparel.present? == false
+      errors.add(:mens_or_womens_apparel, "Please add mens or womens apparel")
+    end
+  end
 end
